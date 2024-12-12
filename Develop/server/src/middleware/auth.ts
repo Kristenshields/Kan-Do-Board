6e84ea748499e241fcd5abadd7b504c21dea298b
Kristen Shields <shieldskristen0@gmail.com>
2024-12-11 19:14:03 -0700

    added env file

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -7,4 +7,16 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.sendStatus(401);
+  }
+
+  const token = authHeader.split(' ');
+  if (token.length !== 2) {
+    return res.sendStatus(401);
+  }
+
+
 };
--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,7 +5,24 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
-};
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.sendStatus(404);
+        }
+
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            return res.sendStatus(401);
+        }
+
+        const newUser = await User.create({
+             username, 
+             password
+             });
+}
 
 const router = Router();
 
